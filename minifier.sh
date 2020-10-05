409347792973d2ac6d5979eebf17058e1e73ab0d
karlwes <karl.westgardh@gmail.com>
2020-10-05 12:58:32 +0200

    add css to minifier

--- minifier.sh
+++ minifier.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 #https://www.npmjs.com/package/minify
 
 npm i minify
 mkdir src/full
 mv src/main.js src/full/main.js
+mv src/main.css src/full/main.css
 minify src/full/main.js > src/main.js
+minify src/full/main.css > src/main.css
